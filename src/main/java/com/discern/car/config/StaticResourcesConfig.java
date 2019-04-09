207ce4c48594b33f8bac294e1a367ce6a2dd77df
caizhy <c494003007@qq.com>
2019-04-10 00:26:50 +0800

    静态

--- src/main/java/com/discern/car/config/StaticResourcesConfig.java
+++ src/main/java/com/discern/car/config/StaticResourcesConfig.java
@@ -2,6 +2,7 @@ package com.discern.car.config;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,8 +15,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class StaticResourcesConfig implements WebMvcConfigurer {
     private static final Log log = LogFactory.getLog(StaticResourcesConfig.class);
 
+    @Value("${img.location}")
+    private String location;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/").addResourceLocations("D://");
+        registry.addResourceHandler("/hzd/pornhub/online").addResourceLocations(location);
     }
 }
