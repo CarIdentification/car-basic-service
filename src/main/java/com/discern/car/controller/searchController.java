da7eeef334cf340bdca07fe105a408e943d147b6
hzd <hzd138@qq.com>
2019-04-10 01:23:33 +0800

    bug修复

--- src/main/java/com/discern/car/controller/searchController.java
+++ src/main/java/com/discern/car/controller/searchController.java
@@ -1,5 +1,6 @@
 package com.discern.car.controller;
 
+import com.alibaba.druid.sql.visitor.functions.Char;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.discern.car.config.RedisService;
@@ -37,259 +38,269 @@ import java.util.Map;
 public class searchController {
 
 
-    @Autowired
-    private IssueService issueService;
+  @Autowired
+  private IssueService issueService;
 
-    @Resource
-    private SearchHistoryService searchHistoryService;
+  @Resource
+  private SearchHistoryService searchHistoryService;
 
-    @Resource
-    private LoginUtil loginUtil;
+  @Resource
+  private LoginUtil loginUtil;
 
-    @Resource
-    private CarBrandService carBrandService;
+  @Resource
+  private CarBrandService carBrandService;
 
-    @Resource
-    private ChineseCharToEn chineseCharToEn;
+  @Resource
+  private ChineseCharToEn chineseCharToEn;
 
-    @Resource
-    private CarService carService;
+  @Resource
+  private CarService carService;
 
-    @Resource
-    private SalesmanService salesmanService;
+  @Resource
+  private SalesmanService salesmanService;
 
-    @Resource
-    private SellShopInfoService sellShopInfoService;
+  @Resource
+  private SellShopInfoService sellShopInfoService;
 
-    @Resource
-    private RedisService redisService ;
+  @Resource
+  private RedisService redisService;
 
-    @Resource
-    private SearchHistoryPicService searchHistoryPicService;
+  @Resource
+  private SearchHistoryPicService searchHistoryPicService;
 
-    @Value("${img.location}")
-    private String location;
+  @Value("${img.location}")
+  private String location;
 
-    @RequestMapping(value = "/pictureDiscern",method = RequestMethod.POST)
-    public ResultDto pictureDiscern(@RequestParam("file") MultipartFile multipartFile, String signature, int serial, int last){
-        User user = loginUtil.cheakLogin(signature);
-        if (multipartFile.isEmpty()) {
-            return new ResultDto("fail","图片不存在！");
-        }else {
-            try {
-                String filePath = new ImageUtil().saveImg(multipartFile,location);
-                SearchHistoryPic searchHistoryPic = null;
-                switch (serial){
-                    case 0:
-                        searchHistoryPic = new SearchHistoryPic();
-                        searchHistoryPic.setUserId(user.getId());
-                        searchHistoryPic.setPic1(location+"\\"+filePath);
-                        redisService.set(signature+"Pic",searchHistoryPic,new Long(1800));
-                        System.out.println("first pic");
-                        break;
-                    case 1:
-                        searchHistoryPic = (SearchHistoryPic)redisService.get(signature+"Pic");
-                        searchHistoryPic.setPic2(location+"\\"+filePath);
-                        redisService.set(signature+"Pic",searchHistoryPic,new Long(1800));
-                        System.out.println("second pic");
-                        break;
-                    case 2:
-                        searchHistoryPic = (SearchHistoryPic)redisService.get(signature+"Pic");
-                        searchHistoryPic.setPic3(location+"\\"+filePath);
-                        redisService.set(signature+"Pic",searchHistoryPic,new Long(1800));
-                        System.out.println("third pic");
-                        break;
-                }
-                if (last==1){
-                    searchHistoryPicService.insertSelective(searchHistoryPic);
-                    System.out.println("discern!");
-                    //调用图像识别api，将图片路径作为参数
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            List<CarDto> carList = null;
-            carList = carService.selectByBrandId(9,1);
-            return new ResultDto("success",carList);
+  @RequestMapping(value = "/pictureDiscern", method = RequestMethod.POST)
+  public ResultDto pictureDiscern(@RequestParam("file") MultipartFile multipartFile,
+      String signature, int serial, int last) {
+    User user = loginUtil.cheakLogin(signature);
+    if (multipartFile.isEmpty()) {
+      return new ResultDto("fail", "图片不存在！");
+    } else {
+      try {
+        String filePath = new ImageUtil().saveImg(multipartFile, location);
+        SearchHistoryPic searchHistoryPic = null;
+        switch (serial) {
+          case 0:
+            searchHistoryPic = new SearchHistoryPic();
+            searchHistoryPic.setUserId(user.getId());
+            searchHistoryPic.setPic1(location + "\\" + filePath);
+            redisService.set(signature + "Pic", searchHistoryPic, new Long(1800));
+            System.out.println("first pic");
+            break;
+          case 1:
+            searchHistoryPic = (SearchHistoryPic) redisService.get(signature + "Pic");
+            searchHistoryPic.setPic2(location + "\\" + filePath);
+            redisService.set(signature + "Pic", searchHistoryPic, new Long(1800));
+            System.out.println("second pic");
+            break;
+          case 2:
+            searchHistoryPic = (SearchHistoryPic) redisService.get(signature + "Pic");
+            searchHistoryPic.setPic3(location + "\\" + filePath);
+            redisService.set(signature + "Pic", searchHistoryPic, new Long(1800));
+            System.out.println("third pic");
+            break;
         }
-
-
-    }
-    /**
-     * 文字搜索
-     * @param searchContext
-     * @return
-     */
-    @RequestMapping("/textSearch")
-    public ResultDto textSearch(String searchContext,String signature){
-        System.out.println("/textSearch");
-        System.out.println("signature :"+signature);
-        //保存搜索记录
-        SearchHistory searchHistory = new SearchHistory();
-        User user = loginUtil.cheakLogin(signature);
-        if (user == null){
-            return new ResultDto("fail","需要授予用户权限！");
+        if (last == 1) {
+          searchHistoryPicService.insertSelective(searchHistoryPic);
+          System.out.println("discern!");
+          //调用图像识别api，将图片路径作为参数
         }
-        //添加搜索记录
-        searchHistory.setUserId(user.getId());
-        searchHistory.setContent(searchContext);
-        searchHistoryService.insertSelective(searchHistory);
-        //获取搜索结果
-        List<Car> list = carService.textSearch(searchContext);
-        Map<String ,Object> map = new HashMap<>();
-        map.put("carList",list);
-        map.put("issue",issueService.selectByTextSearch(searchContext));
-        return new ResultDto("success",map);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+
+      List<CarDto> carList = null;
+      carList = carService.selectByBrandId(9, 1);
+      return new ResultDto("success", carList);
     }
 
-    /**
-     *
-     * @param signature 登陆状态
-     * @return
-     */
-    @ResponseBody
-    @RequestMapping("/textSearchHistory")
-    public ResultDto textSearchHistory(String signature){
-        System.out.println("/textSearchHistory : ");
-        System.out.println("signature :"+signature);
-        //获取用户登陆信息
-        User user = loginUtil.cheakLogin(signature);
-        if (user==null){
-            return new ResultDto("fail","需要授予用户权限！");
-        }else {
-            List<SearchHistory> list = searchHistoryService.selectByUserId(user.getId());
-            return new ResultDto("success",list);
-        }
 
+  }
+
+  /**
+   * 文字搜索
+   */
+  @RequestMapping("/textSearch")
+  public ResultDto textSearch(String searchContext, String signature) {
+    System.out.println("/textSearch");
+    System.out.println("signature :" + signature);
+    //保存搜索记录
+    SearchHistory searchHistory = new SearchHistory();
+    User user = loginUtil.cheakLogin(signature);
+    if (user == null) {
+      return new ResultDto("fail", "需要授予用户权限！");
+    }
+    //添加搜索记录
+    searchHistory.setUserId(user.getId());
+    searchHistory.setContent(searchContext);
+    searchHistoryService.insertSelective(searchHistory);
+    //获取搜索结果
+    List<Car> list = carService.textSearch(searchContext);
+    Map<String, Object> map = new HashMap<>();
+    map.put("carList", list);
+    map.put("issue", issueService.selectByTextSearch(searchContext));
+    return new ResultDto("success", map);
+  }
+
+  /**
+   * @param signature 登陆状态
+   */
+  @ResponseBody
+  @RequestMapping("/textSearchHistory")
+  public ResultDto textSearchHistory(String signature) {
+    System.out.println("/textSearchHistory : ");
+    System.out.println("signature :" + signature);
+    //获取用户登陆信息
+    User user = loginUtil.cheakLogin(signature);
+    if (user == null) {
+      return new ResultDto("fail", "需要授予用户权限！");
+    } else {
+      List<SearchHistory> list = searchHistoryService.selectByUserId(user.getId());
+      return new ResultDto("success", list);
     }
 
-    /**
-     * 高级搜索
-     * @param displacement
-     * @param structure
-     * @param level
-     * @param be_price
-     * @param en_price
-     * @return
-     */
-    @RequestMapping(value = "/advancedSearch",method = RequestMethod.POST)
-    public ResultDto advancedSearch(@RequestParam(value = "displacement",required = false) Integer[] displacement,@RequestParam(value = "structure",required = false) Integer[] structure,@RequestParam(value = "level",required = false) Integer[] level,@RequestParam(value = "Transmission",required = false) Integer[] transmission,@RequestParam(value = "country",required = false) Integer[] country,@RequestParam(value = "production_methods",required = false) Integer[] production_methods,@RequestParam(value = "energy",required = false) Integer[] energy,@RequestParam(value = "driving_method",required = false) Integer[] driving_method,@RequestParam(value = "seat",required = false) Integer[] seat,@RequestParam(value = "be_price",required = false) Float be_price,@RequestParam(value = "en_price",required = false) Float en_price){
-        System.out.println(level);
-        System.out.println(structure);
-        List<Car> list = carService.advancedSelect(displacement, structure, level, transmission, country, production_methods,  energy,  driving_method, seat,  be_price, en_price);
+  }
 
-        return new ResultDto("success",list);
+  /**
+   * 高级搜索
+   */
+  @RequestMapping(value = "/advancedSearch", method = RequestMethod.POST)
+  public ResultDto advancedSearch(
+      @RequestParam(value = "displacement", required = false) Integer[] displacement,
+      @RequestParam(value = "structure", required = false) Integer[] structure,
+      @RequestParam(value = "level", required = false) Integer[] level,
+      @RequestParam(value = "Transmission", required = false) Integer[] transmission,
+      @RequestParam(value = "country", required = false) Integer[] country,
+      @RequestParam(value = "production_methods", required = false) Integer[] production_methods,
+      @RequestParam(value = "energy", required = false) Integer[] energy,
+      @RequestParam(value = "driving_method", required = false) Integer[] driving_method,
+      @RequestParam(value = "seat", required = false) Integer[] seat,
+      @RequestParam(value = "be_price", required = false) Float be_price,
+      @RequestParam(value = "en_price", required = false) Float en_price) {
+    System.out.println(level);
+    System.out.println(structure);
+    List<Car> list = carService
+        .advancedSelect(displacement, structure, level, transmission, country, production_methods,
+            energy, driving_method, seat, be_price, en_price);
+
+    return new ResultDto("success", list);
+  }
+
+  /**
+   * 提供第一层品牌
+   */
+  @RequestMapping(value = "/getBrands", method = RequestMethod.GET)
+  public ResultDto getBrands() {
+    System.out.println("/getBrands : ");
+
+    List<CarBrand> list = carBrandService.selectFatherBrands();
+    Map map = new HashMap();
+    for (int i = 0; i < 26; i++) {
+      map.put((char) (65 + i), new ArrayList<CarBrand>());
     }
 
-    /**
-     * 提供第一层品牌
-     * @return
-     */
-    @RequestMapping(value = "/getBrands",method = RequestMethod.GET)
-    public ResultDto getBrands(){
-        System.out.println("/getBrands : ");
+    for (CarBrand carBrand : list) {
+      String firstLetterString = chineseCharToEn.getFirstLetter(carBrand.getName());
 
-        List<CarBrand> list = carBrandService.selectFatherBrands();
-        Map map =new HashMap();
-        for (int i = 0 ; i < 24 ; i++ ){
-            map.put((char)(65+i),new ArrayList<CarBrand>());
-        }
+      Character c = null;
 
-        for (CarBrand carBrand : list){
-            char c = chineseCharToEn.getFirstLetter(carBrand.getName()).charAt(0);
-            ((List<CarBrand>)map.get((char)(c-32))).add(carBrand);
+      c = firstLetterString.toLowerCase().charAt(0);
+      if ("讴".equals(c)) {
+        ((List<CarBrand>) map.get('O')).add(carBrand);
+        continue;
+      }
+      try {
+        ((List<CarBrand>) map.get((char) (c - 32))).add(carBrand);
+      } catch (NullPointerException e) {
+        e.printStackTrace();
+        System.out.println("=================字符 " + c);
+      }
 
-        }
-        return new ResultDto("success",map);
 
     }
+    return new ResultDto("success", map);
 
-    /**
-     * 获取单个品牌的信息
-     * @param id
-     * @return
-     */
-    @RequestMapping(value = "/getBrand",method = RequestMethod.GET)
-    public ResultDto getBrand(Integer id){
-        CarBrand carBrand = carBrandService.selectByPrimaryKey(id);
-        return new ResultDto("success",carBrand);
+  }
+
+  /**
+   * 获取单个品牌的信息
+   */
+  @RequestMapping(value = "/getBrand", method = RequestMethod.GET)
+  public ResultDto getBrand(Integer id) {
+    CarBrand carBrand = carBrandService.selectByPrimaryKey(id);
+    return new ResultDto("success", carBrand);
+  }
+
+  /**
+   * 提供一层品牌以下的二层品牌与三层品牌
+   */
+  @RequestMapping(value = "/getSonBrands", method = RequestMethod.GET)
+  public ResultDto getSonBrands(Integer id) {
+    System.out.println("/getBrands : ");
+    List<BrandDto> list = carBrandService.selectSonBrands(id);
+    return new ResultDto("success", list);
+  }
+
+  /**
+   * 提供第三层品牌以下的所有汽车
+   */
+  @RequestMapping(value = "/getCars", method = RequestMethod.GET)
+  public ResultDto getCars(Integer id, Integer pageNum) {
+    System.out.println("/getCars : ");
+    List<CarDto> list = carService.selectByBrandId(id, pageNum);
+    Map<String, Object> map = new HashMap<>();
+    map.put("cars", list);
+    if (list.size() != 0) {
+      pageNum++;
+    } else {
+      pageNum = -1;
     }
+    map.put("pageNum", pageNum);
+    return new ResultDto("success", map);
+  }
 
-    /**
-     * 提供一层品牌以下的二层品牌与三层品牌
-     * @param id
-     * @return
-     */
-    @RequestMapping(value = "/getSonBrands",method = RequestMethod.GET)
-    public ResultDto getSonBrands(Integer id){
-        System.out.println("/getBrands : ");
-        List<BrandDto> list = carBrandService.selectSonBrands(id);
-        return new ResultDto("success",list);
+  /**
+   * 获取车信息
+   */
+  @RequestMapping(value = "/getCar", method = RequestMethod.GET)
+  public ResultDto getCar(@RequestParam(name = "ids", required = false) String idArrayString,
+      @RequestParam(name = "id", required = false) Integer id,
+      double latitude, double longitude) {
+    if (idArrayString == null && id == null) {
+      return ResultDto.ERR_PARAM;
     }
+    if (idArrayString != null) {
+      JSONArray jsonArray = JSON.parseArray(idArrayString);
+      List<CarDto> carDtoArrayList;
+      List<Integer> ids = new ArrayList<>();
+      for (int i = 0; i < jsonArray.size(); i++) {
+        ids.add(jsonArray.getIntValue(i));
+      }
+      carDtoArrayList = carService.selectByPrimaryKeys(ids);
 
-    /**
-     * 提供第三层品牌以下的所有汽车
-     * @param id
-     * @return
-     */
-    @RequestMapping(value = "/getCars",method = RequestMethod.GET)
-    public ResultDto getCars(Integer id,Integer pageNum){
-        System.out.println("/getCars : ");
-        List<CarDto> list = carService.selectByBrandId(id,pageNum);
-        Map<String,Object> map = new HashMap<>();
-        map.put("cars",list);
-        if (list.size()!=0){
-            pageNum++;
-        }else {
-            pageNum=-1;
-        }
-        map.put("pageNum",pageNum);
-        return new ResultDto("success",map);
+      for (CarDto tmpDto : carDtoArrayList) {
+        //TODO: 查询根brandId
+        tmpDto.setSaleShops(sellShopInfoService.selectAroundSellShopByBrandId(latitude, longitude,
+            tmpDto.getCarBrand()));
+      }
+      return new ResultDto("success", carDtoArrayList);
+    } else {
+      CarDto car = carService.selectByPrimaryKey(id);
+      //TODO: 查询根brandId
+      car.setSaleShops(sellShopInfoService.selectAroundSellShopByBrandId(latitude, longitude,
+          car.getCarBrand()));
+      return new ResultDto("success", car);
     }
+  }
 
-    /**
-     * 获取车信息
-     */
-    @RequestMapping(value = "/getCar",method = RequestMethod.GET)
-    public ResultDto getCar(@RequestParam(name = "ids",required = false) String idArrayString,
-        @RequestParam(name = "id",required = false) Integer id,
-        double latitude,double longitude){
-        if(idArrayString == null && id == null){
-            return ResultDto.ERR_PARAM;
-        }
-        if(idArrayString != null){
-            JSONArray jsonArray = JSON.parseArray(idArrayString);
-            List<CarDto> carDtoArrayList;
-            List<Integer> ids = new ArrayList<>();
-            for(int i = 0;i < jsonArray.size();i++){
-                ids.add(jsonArray.getIntValue(i));
-            }
-            carDtoArrayList = carService.selectByPrimaryKeys(ids);
-
-            for (CarDto tmpDto : carDtoArrayList){
-                //TODO: 查询根brandId
-                tmpDto.setSaleShops(sellShopInfoService.selectAroundSellShopByBrandId(latitude,longitude,
-                    tmpDto.getCarBrand()));
-            }
-            return new ResultDto("success",carDtoArrayList);
-        }else{
-            CarDto car = carService.selectByPrimaryKey(id);
-            //TODO: 查询根brandId
-            car.setSaleShops(sellShopInfoService.selectAroundSellShopByBrandId(latitude,longitude,
-                car.getCarBrand()));
-            return new ResultDto("success",car);
-        }
-    }
-
-    /**
-     * 获取指定车型的销售人员
-     * @param brandId
-     * @return
-     */
-    @RequestMapping(value = "/getSalesman",method = RequestMethod.GET)
-    public ResultDto getSalesman(Integer brandId){
-        List<SalesmanDto> list = salesmanService.selectByBrandId(brandId);
-        return new ResultDto("success",list);
-    }
+  /**
+   * 获取指定车型的销售人员
+   */
+  @RequestMapping(value = "/getSalesman", method = RequestMethod.GET)
+  public ResultDto getSalesman(Integer brandId) {
+    List<SalesmanDto> list = salesmanService.selectByBrandId(brandId);
+    return new ResultDto("success", list);
+  }
 }
