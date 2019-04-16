2e73f1fac2ef2d3807d657de9fe012f91b243f35
hzd <hzd138@qq.com>
2019-04-16 12:07:22 +0800

    文章相关接口性能优化

--- src/main/java/com/discern/car/util/BeanUtil.java
+++ src/main/java/com/discern/car/util/BeanUtil.java
@@ -6,7 +6,7 @@ import com.google.common.collect.Maps;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.cglib.beans.BeanCopier;
 
